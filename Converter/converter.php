e90d03ed38855c34787c590b87a2063716ee24f6
Ondřej Teplický <teplion@student.uzlabina.cz>
2023-01-24 10:27:12 +0100

    konvertor

---
+++ Converter/converter.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="cs">
+    <head>
+        <meta charset="UTF-8">
+        <title>Converter</title>
+    </head>
+    <body>
+        <form>
+            <label for="numberId">Číslo </label>
+        </form>
+    </body>
+</html>
