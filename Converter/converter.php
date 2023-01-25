e87a633138860b35b0d0e091bc57523e36d63a6d
BrewsLee <ondratep@gmail.com>
2023-01-25 22:27:19 +0100

    Konvertor 1.0

--- Converter/converter.php
+++ Converter/converter.php
@@ -9,17 +9,33 @@
         <form>
             <label for="numberId">Číslo </label>
             <input type="number" placeholder="V desítkové soustavě" name="num" id="numberId">
+            <br>
             <label for="ConvertTo">Soustava</label>
             <input type="number" name="convert" id="numberId">
+            <br>
             <button type="submit">Odeslat</button>
         </form>
         <?php
-            $num = $_GET['num'];
+            $num = (string)$_GET['num'];
             $convert = $_GET['convert'];
 
-            echo $num;
-            echo $convert;
+            echo 'Číslo: ' . $num . '<br>';
+            echo 'Do soustavy: ' . $convert . '<br>';
 
+            echo 'Výsledek: ' . strrev(convert($num, $convert));
+
+            function convert($num, $convert) {
+                $converted = "";
+                $remainder;
+                while($num > 0) {
+                    $remainder = $num%$convert;
+                    $num = intval(($num - $remainder)/$convert);
+                    if($remainder > 9)
+                        $remainder = chr($remainder+55);
+                    $converted .= $remainder;
+                }
+                return $converted;
+            }
         ?>
     </body>
 </html>
