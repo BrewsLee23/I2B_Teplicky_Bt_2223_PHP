4476db85beccedfda19680545ab7c87ad0d2b3d4
BrewsLee <ondratep@gmail.com>
2023-01-30 22:11:12 +0100

    Sudoku Start

---
+++ Sudoku/sudoku.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+
+<html lang="cs">
+    <head>
+        <meta charset="UTF-8">
+        <title>Sudoku</title>
+        <style>
+            .squares {
+                display: flex;
+                flex-direction: column;
+            }
+
+            .block {
+                display: inline-flex;
+                flex-direction: row;
+                border: solid black 3px;
+                border-collapse: collapse;
+            }
+
+            .row-blocks {
+                display: flex;
+                flex-direction: row;
+            }
+
+            .sudoku {
+                display: flex;
+                flex-direction: column;
+            }
+
+            input {
+                margin: 0;
+                height: 1.5rem;
+                width: 1.5rem;
+                text-align: right;
+                font-size: 1rem;
+            }
+
+            input:focus {
+                outline: none;
+            }
+
+            input:hover {
+                cursor: default;
+            }
+
+        </style>
+    </head>
+    <body>
+        <form>
+            <div class="sudoku">
+                <div class="row-blocks">
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                </div>
+                <div class="row-blocks">
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                </div>
+                <div class="row-blocks">
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                    <div class="block">
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                        <div class="squares">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                            <input type="number" min="1" max="9" step="1">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+            <?php
+                
+            ?>
+
+    </body>
+</html>
