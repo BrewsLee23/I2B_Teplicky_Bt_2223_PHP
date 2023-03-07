cf8e994adff6689902717e5251b2a7c700bbe9b7
Ondřej Teplický <teplion@student.uzlabina.cz>
2023-03-07 08:01:16 +0100

    h

--- 05_hodina/dashboard.php
+++ 05_hodina/dashboard.php
@@ -1,9 +1,13 @@
 <?php
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
 
-    const USERNAME = 'fresh';
-    const PASSWORD = 'ABCabc123';
+    require_once 'config.php';
 
     session_start();
+
+    $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_USERNAME, DB_USERNAME, DB_PASSWORD);
 
     if (issset($_COOKIE['count'])) {
         setcookie('count', 0, time() + 3600 * 24 * 30 , '/'); // CTRL + click na setcookie
@@ -13,11 +17,21 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        function login($username, $password){
-            return $username == USERNAME && $password == PASSWORD;
+        function login (string $username, string $password, $db): bool {
+            // return $username == USERNAME && $password == PASSWORD;
+            $query = $db->query('SELECT * FROM users');
+            $users = $query->fetchAll();
+
+            for ($i = 0; $i < count($users); $i++) {
+                $user = $users[$i];
+                if($username == $user['username'] && $password == $user['password']) {
+                    return true;
+                }
+            }
+            return false;
         }
 
-        if (login($username, $password)) {
+        if (login($username, $password, $db)) {
             echo 'Zadali jste správné údaje';
             $_SESSION['user'] = $username;
             $_SESSION['count'] = 0;
